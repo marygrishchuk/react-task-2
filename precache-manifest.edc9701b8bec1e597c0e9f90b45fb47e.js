16481046785aaf88bde984644ea498735445b76d
Maria Grishchuk <mary.grishchuk1@gmail.com>
2020-10-25 19:00:56 +0300

    Updates

---
+++ precache-manifest.edc9701b8bec1e597c0e9f90b45fb47e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f475cc7b08391a2a2cb6a59d4d80fc8b",
+    "url": "/react-task-2/index.html"
+  },
+  {
+    "revision": "c57898ba60d33c613400",
+    "url": "/react-task-2/static/css/main.b249b83c.chunk.css"
+  },
+  {
+    "revision": "39733bc4aa8c3040504e",
+    "url": "/react-task-2/static/js/2.3a73f86d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-task-2/static/js/2.3a73f86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57898ba60d33c613400",
+    "url": "/react-task-2/static/js/main.bd7551ff.chunk.js"
+  },
+  {
+    "revision": "1466ea4a762e46b6fe89",
+    "url": "/react-task-2/static/js/runtime-main.b7795bd0.js"
+  }
+]);
